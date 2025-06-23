fe5b6248d4c420bd5c8979f26ebe9df794b87677
Abhishek Kumar <146465745+42Abhishek@users.noreply.github.com>
2025-06-23 09:14:37 +0530

    Update userAuth.js

--- Backend/src/controllers/userAuth.js
+++ Backend/src/controllers/userAuth.js
@@ -20,7 +20,12 @@ const register = async (req,res) => {
         const user = await User.create(req.body);
 
         const token = jwt.sign({_id : user._id,emailId : emailId,role:"user"},process.env.JWT_KEY,{expiresIn : 60 * 60 });
-        res.cookie('token',token,{maxAge : 60 * 60 * 1000});
+        res.cookie('token',token,{
+         httpOnly: true,
+         secure: true,
+         sameSite: "None",
+         maxAge : 60 * 60 * 1000
+        });
         
  const reply = {
             firstName : user.firstName,
@@ -56,7 +61,12 @@ const login = async (req,res) => {
             throw new Error("Invalid Credentials");
 
          const token = jwt.sign({_id : user._id,emailId : emailId,role : user.role},process.env.JWT_KEY,{expiresIn : 60 * 60 });
-        res.cookie('token',token,{maxAge : 60 * 60 * 1000});
+        res.cookie('token',token,{
+         httpOnly: true,
+         secure: true,
+         sameSite: "None",
+         maxAge : 60 * 60 * 1000
+        });
 
         const reply = {
             firstName : user.firstName,
